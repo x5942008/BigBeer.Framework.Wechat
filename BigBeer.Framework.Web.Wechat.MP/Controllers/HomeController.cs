271eee609c18f3ab7e6d0156c2c4904d12b5996e
吴纪雄 <243174414@qq.com>
2018-01-31 20:27:25 +0800

    微信开发

---
+++ BigBeer.Framework.Web.Wechat.MP/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BigBeer.Framework.Web.Wechat.MP.Controllers
+{
+    public class HomeController
+    {
+    }
+}
